59b9388e79ff5c2adbdfd2cf978bc4dc5ffe3088
AlcashZone <112493283+Alcashzone@users.noreply.github.com>
2025-02-13 03:07:55 +0500

    Update rescuestories.py

--- rescuestories.py
+++ rescuestories.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "Enter your API Key here"
+API_KEY = "AIzaSyC9Cf0P61P-g7IRpa1wB7NT4bR6qrQmEqg"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
